5332838a61f62044766f1d945f497dece894294e
Agouzoul Reda <redaagouzoul23@gmail.com>
2026-01-22 22:11:36 +0100

    Refactor start procedure and add consult statements
    Updated the start procedure to include new consult statements and modified the welcome message.

--- recommandation_metiers/main.pl
+++ recommandation_metiers/main.pl
@@ -1,14 +1,19 @@
-start :-
-    % Nettoyage des anciennes réponses en mémoire vive
-    retractall(oui(_)),
-    retractall(non(_)),
-    retractall(competence(_)),  % <-- AJOUTER CETTE LIGNE
-    
-    write('************************************************'), nl,
-    write('*   BIENVENUE SUR L\'IA D\'ORIENTATION MASTER BD   *'), nl,
-    write('************************************************'), nl,
-    write('Conseil : Répondez par "oui." ou "non."'), nl, nl,
-    
-    % On lance l'exécution du moteur
-    executer_moteur.
+% main.pl
+:- consult('faits.pl').
+:- consult('questions.pl').
+:- consult('regles.pl').
+:- consult('moteur.pl').
 
+start :-
+    % Nettoyage mémoire vive
+    retractall(competence(_)),
+    retractall(domaine_interesse(_)),
+    
+    write('-------------------------------------------'), nl,
+    write('    SYSTEME EXPERT ORIENTATION IT v3.0     '), nl,
+    write('-------------------------------------------'), nl,
+    
+    % Lancement du moteur
+    executer_moteur,
+    
+    nl, write('Consultation terminee.'), nl.
